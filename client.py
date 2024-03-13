a5883efe6ba342d1ea7b4bb7a19b291e9ca9062e
Erik <eerriikknrp@gmail.com>
2024-03-14 02:42:12 +0800

    Basic Client Setup

--- client.py
+++ client.py
@@ -1,1 +1,26 @@
 import socket
+from number_game import GuessResult
+from constants import HOST, PORT
+
+guess_result = 999
+
+while guess_result != GuessResult.GUESS_CORRECT:
+    with socket.socket() as s:
+        s.connect((HOST, PORT))
+        while not guess:
+            guess = input("What number would you like to guess?\n")
+            try:
+                int(guess)
+            except:
+                print("Please enter a valid Integer")
+        s.sendall(int.to_bytes(guess))
+        data = int.from_bytes(s.recv(2))
+        if data == GuessResult.GUESS_CORRECT:
+            print(f"Correct! The number was {data}")
+            break
+        if data == GuessResult.GUESS_TOO_LARGE:
+            print(f"Your guess was too large :(")
+        if data == GuessResult.GUESS_TOO_SMALL:
+            print(f"Your guess was too small :(")
+        if data == GuessResult.GUESS_CORRECT:
+            print(f"There was an error with your guess on the server side")
