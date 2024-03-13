0a2a6ade906bb4fbca0ebfab6406ca97970b4970
Erik <eerriikknrp@gmail.com>
2024-03-13 23:07:50 +0800

    Basic NumberGame Class Finished

--- number_game.py
+++ number_game.py
@@ -1,4 +1,27 @@
 class NumberGame:
     def __init__(self, secret):
         self.secret = secret
+        self.guesses = 0
+    
+    def check(self, guess):
+        if not isinstance(guess, int):
+            raise ValueError("Guess is not integer. Guess: " + str(guess))
+        self.guesses += 1
+        if self.secret == guess:
+            return 0
+        if self.secret > guess:
+            return 1
+        if self.secret < guess:
+            return -1
+
+        
+    def get_guesses(self):
+        return self.guesses
+    
+    def clear_guesses(self):
+        self.guesses = 0
+        return 0
+    
+
+
     
